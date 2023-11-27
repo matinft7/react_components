faae106a0ec968fc1352e1ba23301ebb0a80ed37
matinft7 <matinft27@gmail.com>
2023-11-27 11:56:25 +0330

    nav links added

--- src/app/components/navbar.js
+++ src/app/components/navbar.js
@@ -1,7 +1,7 @@
 // Navigation bar with brand name
 export default function NavBar({ appname , data , classnames}){
     var key = 0;
-    const listData = data?.map(d => <li key={ key=key+1 }>{ d }</li>);
+    const listData = data?.map(d => <li key={ key=key+1 }><a href={ d.href }>{ d.name }</a></li>);
     const classnameList = classnames.toString().replace(","," ");
     return(
         <nav className={ classnameList }>
